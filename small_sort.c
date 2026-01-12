cfff767cf8413c32e23de7ebdd5e278fe067c0a2
Weng Man Cheung <wcheung@3-H-5.42heilbronn.de>
2026-01-12 15:05:14 +0100

    sort two three

---
+++ small_sort.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   small_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wcheung <wcheung@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/11 11:07:34 by wcheung           #+#    #+#             */
+/*   Updated: 2026/01/12 15:04:36 by wcheung          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sort_two(t_node **stack_a)
+{
+	int	first;
+	int	second;
+
+	first = (*stack_a)->index;
+	second = (*stack_a)->next->index;
+	if (first > second)
+		sa(stack_a);
+}
+
+void	sort_three(t_node **stack_a)
+{
+	int	first;
+	int	second;
+	int	third;
+
+	first = (*stack_a)->index;
+	second = (*stack_a)->next->index;
+	third = (*stack_a)->next->next->index;
+	if (first > second && first > third)
+		ra(stack_a);
+	else if (second > first && second > third)
+		rra(stack_a);
+	sort_two(stack_a);
+}
+
+void	sort_four(t_node **stack_a)
+{
+	
+}
+void	sort_five(t_node **stack_a)
+{
+
+}
+// 3	3	2	2	1	1
+// 2	1	3	1	3	2
+// 1	2	1	3	2	3
