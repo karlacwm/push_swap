be21e77b73dc2bd003e3ed1c484dd2fc91097b89
Weng Man Cheung <wcheung@3-D-3.42heilbronn.de>
2026-01-16 13:06:21 +0100

    parsing

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: wcheung <wcheung@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 11:26:24 by wcheung           #+#    #+#             */
-/*   Updated: 2026/01/11 20:28:45 by wcheung          ###   ########.fr       */
+/*   Updated: 2026/01/16 13:05:46 by wcheung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,18 +40,66 @@ int	check_duplicates(t_node *a, int n)
 	return (0);
 }
 
-t_node	*stack_new(int content)
+long	ft_atol(const char *str)
+{
+	long	result;
+	int		sign;
+	int		i;
 
-void	build_stack_a()
-{
-	while ()
-{
-	if (check_integers())
-		error;
-	if (check_duplicates(*a, (int)n))
-		error;
-	ft_lstaddback
-	next
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
+
+t_node	*put_value(int content)
+{
+	t_node	*new;
+
+	new = malloc(sizeof(t_node));
+	if (!new)
+		return (NULL);
+	new->value = content;
+	new->index = -1;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
+}
+
+void	build_stack_a(t_node **a, char **argv, int argc)
+{
+	int		i;
+	long	n;
+	t_node	*new_node;
+
+	i = 1;
+	while (argv[i])
+	{
+		if (check_integers(argv[i]))
+			free_error(a);
+		n = ft_atol(argv[i]);
+		if (n > 2147483647 || n < -2147483648)
+			free_error(a);
+		if (check_duplicates(*a, (int)n))
+			free_error(a);
+		new_node = put_value();
+		if (!new_node)
+			free_error(a);
+		// add to bottom of stack ft_lstaddback
+		i++;
+	}
 }
 
