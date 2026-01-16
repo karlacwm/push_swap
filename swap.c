558a8691def3dc14bf2b6616768f8c84522eac57
Weng Man Cheung <wcheung@3-D-3.42heilbronn.de>
2026-01-16 16:44:13 +0100

    swap

--- swap.c
+++ swap.c
@@ -6,7 +6,7 @@
 /*   By: wcheung <wcheung@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 19:45:06 by wcheung           #+#    #+#             */
-/*   Updated: 2026/01/09 20:01:35 by wcheung          ###   ########.fr       */
+/*   Updated: 2026/01/16 16:43:56 by wcheung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,47 @@ ss : sa and sb at the same time
 
 #include "push_swap.h"
 
-void	sa(t_node **stack_a)
+void	swap(t_node **a)
 {
 	t_node	*first;
 	t_node	*second;
 
-	if (!*stack_a || !(*stack_a)->next)
+	if (!*a || !(*a)->next)
 		return ;
-	first = *stack_a;
+	first = *a;
 	second = first->next;
 	first->next = second->next;
+	if (second->next)
+		second->next->prev = first;
 	second->next = first;
-	*stack_a = second;
+	second->prev = NULL;
+	first->prev = second;
+	*a = second;
 	write(1, "sa\n", 3);
 }
 
+void	sb(t_node **b)
+{
+	t_node	*first;
+	t_node	*second;
 
+	if (!*b || !(*b)->next)
+		return ;
+	first = *b;
+	second = first->next;
+	first->next = second->next;
+	if (second->next)
+		second->next->prev = first;
+	second->next = first;
+	second->prev = NULL;
+	first->prev = second;
+	*b = second;
+	write(1, "sb\n", 3);
+}
+
+void	ss(t_node **a, t_node **b)
+{
+	// sa(a); // but it already writes sa\n
+	// sb(b);
+	// write(1, "ss\n", 3);
+}
