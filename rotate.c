9760d05a9979b1ad060e8dfecddb9663e364bbd9
Weng Man Cheung <wcheung@3-D-3.42heilbronn.de>
2026-01-16 19:52:33 +0100

    rotate

--- rotate.c
+++ rotate.c
@@ -6,7 +6,7 @@
 /*   By: wcheung <wcheung@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 19:58:36 by wcheung           #+#    #+#             */
-/*   Updated: 2026/01/09 20:01:24 by wcheung          ###   ########.fr       */
+/*   Updated: 2026/01/16 19:52:01 by wcheung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,37 @@ rr : ra and rb at the same time.
 
 #include "push_swap.h"
 
+void	rotate(t_node **stack)
+{
+	t_node	*first;
+	t_node	*last;
+
+	if (!*stack)
+		return ;
+	first = *stack;
+	last = find_last_node(stack);
+	*stack = first->next;
+	(*stack)->prev = NULL;
+	last->next = first;
+	first->prev = last;
+	first->next = NULL;
+}
+
+void	ra(t_node **a)
+{
+	rotate(a);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_node **b)
+{
+	rotate(b);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_node **a, t_node **b)
+{
+	rotate(a);
+	rotate(b);
+	write(1, "rr\n", 3);
+}
