b03ae54599c42fb7ee3e69d8da576a48ed7a1eaa
Weng Man Cheung <wcheung@3-D-6.42heilbronn.de>
2026-01-18 16:10:15 +0100

    print stacks

---
+++ print_stacks.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_stacks.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wcheung <wcheung@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/11 11:12:31 by wcheung           #+#    #+#             */
+/*   Updated: 2026/01/16 15:26:44 by wcheung          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	print_stacks(t_node *a, t_node *b)
+{
+	printf("Stack A: \n");
+	while (a)
+	{
+		printf("[%d] %d\n", a->index, a->value);
+		a = a->next;
+	}
+	printf("Stack B: \n");
+	while (b)
+	{
+		printf("[%d] %d\n", b->index, b->value);
+		b = b->next;
+	}
+}
