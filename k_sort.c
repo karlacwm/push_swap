adb9d47dcb19ad8c2391488bf18cc50dc059242f
Weng Man Cheung <wcheung@3-A-3.42heilbronn.de>
2026-01-19 15:08:52 +0100

    k sort step 2

--- k_sort.c
+++ k_sort.c
@@ -6,7 +6,7 @@
 /*   By: wcheung <wcheung@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 15:04:20 by wcheung           #+#    #+#             */
-/*   Updated: 2026/01/19 10:58:48 by wcheung          ###   ########.fr       */
+/*   Updated: 2026/01/19 13:04:41 by wcheung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ int	chunk_size(int size)
 		return (15);
 	else if (size <= 500)
 		return (33);
+	return (50);
 }
 
-void	k_sort(t_node **a, t_node **b, int size)
+void	k_sort_chunks(t_node **a, t_node **b, int size)
 {
 	int	chunk;
 	int	i;
@@ -28,9 +29,10 @@ void	k_sort(t_node **a, t_node **b, int size)
 
 	chunk = chunk_size(size);
 	i = 0;
-	range = i + chunk;
+
 	while (*a)
 	{
+		range = i + chunk;
 		if ((*a)->index <= i)
 		{
 			pb(a, b);
@@ -47,6 +49,29 @@ void	k_sort(t_node **a, t_node **b, int size)
 	}
 }
 
+void	refine_chunks(t_node **a, t_node **b)
+{
+	int	max_index;
+
+	max_index = stack_size(*b) - 1;
+	while (*b)
+	{
+		if ((*b)->index == max_index)
+		{
+			pa(a, b);
+			max_index--;
+		}
+		else
+			rrb(b);
+	}
+}
+
+void	k_sort(t_node **a, t_node **b, int size)
+{
+	k_sort_chunks(a, b, size);
+	refine_chunks(a, b);
+}
+
 // push int from a to b in roughly sorted chunks
 // +/- k is the range of the correct position = k_sort
 // need chunk size
@@ -59,36 +84,40 @@ void	k_sort(t_node **a, t_node **b, int size)
 
 // stack a:
 // [3] 4
-// [6] 8
+// [6] 7
 // [0] 1
 // [5] 6
-// [8] 10
+// [8] 9
 // [2] 3
 // [1] 2
 // [4] 5
-// [7] 9
+// [7] 8
 
 // chunk = 3
 // i = 3
 // range = i + chunk
 
 // stack a:	b:
-// 			[8] 10
-// 			[7] 9
+// 			[8] 9
+// 			[7] 8
 // 			[5] 6
 // 			[3] 4
 // 			[0] 1
 // 			[2] 3
 // 			[1] 2
 // 			[4] 5
-// 			[6] 8
+// 			[6] 7
 
-// i=0 range = 3 [3] 4  pb
-// i=1 range = 4 [6] 8  ra
-// i=2 range = 5 [0] 1  pb rb
-// i=3 range = 6 [5] 6  pb
-// i=4 range = 7 [8] 10 ra
+// i=0 range = 3 [3] 4 pb
+// i=1 range = 4 [6] 7 ra
+// i=2 range = 5 [0] 1 pb rb
+// i=3 range = 6 [5] 6 pb
+// i=4 range = 7 [8] 9 ra
 // pb rb
 // pb rb
 // pb rb
 // pb
+
+// max index = size -1 = 8
+// stack a:	b:
+
