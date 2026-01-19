52fc70f42b4f159fe43be399a7f8d710bfeb8cd6
Weng Man Cheung <wcheung@3-D-6.42heilbronn.de>
2026-01-19 10:59:20 +0100

    k sort chunks

--- k_sort.c
+++ k_sort.c
@@ -6,7 +6,7 @@
 /*   By: wcheung <wcheung@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 15:04:20 by wcheung           #+#    #+#             */
-/*   Updated: 2026/01/18 15:56:32 by wcheung          ###   ########.fr       */
+/*   Updated: 2026/01/19 10:58:48 by wcheung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,73 @@ int	chunk_size(int size)
 
 void	k_sort(t_node **a, t_node **b, int size)
 {
-	// ra when int is too big
-	// pb when int is within chunk
-	// pb rb when int is the smallest
 	int	chunk;
+	int	i;
+	int	range;
 
 	chunk = chunk_size(size);
-
+	i = 0;
+	range = i + chunk;
+	while (*a)
+	{
+		if ((*a)->index <= i)
+		{
+			pb(a, b);
+			rb(b);
+			i++;
+		}
+		else if ((*a)->index <= range)
+		{
+			pb(a, b);
+			i++;
+		}
+		else
+			ra(a);
+	}
 }
 
-// push int from a to b in roughly sorted chunks = k_sort
+// push int from a to b in roughly sorted chunks
+// +/- k is the range of the correct position = k_sort
 // need chunk size
+// rotate stack a when int is too big for current chunk
 // in many algorithms, most efficient way: divide a set by its square root
 // for push swap, sqaure root x 1.5: lowest move count
+// ra when int is too big
+// pb when int is in the bigger half
+// pb rb when int is in the smaller half in chunk
+
+// stack a:
+// [3] 4
+// [6] 8
+// [0] 1
+// [5] 6
+// [8] 10
+// [2] 3
+// [1] 2
+// [4] 5
+// [7] 9
+
+// chunk = 3
+// i = 3
+// range = i + chunk
+
+// stack a:	b:
+// 			[8] 10
+// 			[7] 9
+// 			[5] 6
+// 			[3] 4
+// 			[0] 1
+// 			[2] 3
+// 			[1] 2
+// 			[4] 5
+// 			[6] 8
+
+// i=0 range = 3 [3] 4  pb
+// i=1 range = 4 [6] 8  ra
+// i=2 range = 5 [0] 1  pb rb
+// i=3 range = 6 [5] 6  pb
+// i=4 range = 7 [8] 10 ra
+// pb rb
+// pb rb
+// pb rb
+// pb
